e750547405f9943f949ad0cc50746eb1b5d97212
ChunXela <johansson.axel@protonmail.com>
2022-10-21 01:22:50 +0200

    Changing gui layout

--- src/GUI.java
+++ src/GUI.java
@@ -7,7 +7,7 @@ public class GUI {
     int w= 1920;
     int h =1080;
     int bw=300;
-    int bh=100;
+    int bh=50;
     JFrame frame;
     JPanel panel;
     JButton login;
@@ -23,40 +23,52 @@ public class GUI {
     
     void GUIMenu(){
 
-// make the frame
+        // make the frame
         frame = new JFrame("Menu");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-// make the panel
+
+        // make the panel
         panel  = new JPanel();
         panel.setPreferredSize(new Dimension(w, h));
+
         // setting a border
         panel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
-        // setting up a grid layout
-        panel.setLayout(new GridLayout(4, 4));
+
+        //Setting the layout to null
+        panel.setLayout(null);
+        
         // making the frame Fullscreen
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
         frame.setUndecorated(true);
 
         // Putting in the text NTI BANKEN and putting it in the center
-        title = new JLabel("NTI BANKEN", SwingConstants.CENTER);
+        title = new JLabel("NTI BANKEN");
+        title.setBounds(760, 50, 400, 100);
         title.setForeground(Color.WHITE);
         title.setFont(new Font("Serif", Font.BOLD, 60));
         panel.add(title);
+
         //Making a button that says login
         login = new JButton("Login");  
-        login.setSize(bw, bh);
+        login.setBounds(100, 900, bw, bh);
         login.setFont(new Font("Serif", Font.BOLD, 30));
         panel.add(login);
+
         //Making a button that says Create bank account
-        createBank = new JButton("Create bank account");  
+        createBank = new JButton("Create bank account"); 
+        createBank.setBounds(1500, 900, bw, bh); 
         createBank.setFont(new Font("Serif", Font.BOLD, 30));
-        createBank.setSize(bw, bh);
         panel.add(createBank);
+
+
         //setting the background of the application to black
         panel.setBackground(Color.BLACK);
+
         //putting the panel in the frame
         frame.getContentPane().add(panel);
         
+
+
         frame.pack();   
         frame.setVisible(true);
     }
